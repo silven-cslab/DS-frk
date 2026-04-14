88987459bf7f7d2fec937cee4dbd41f0308728b5
silven-mohan <silven667@gmail.com>
2026-04-14 23:17:21 +0530

    Completed Insertion.

--- LList/DLList/ops/insert.c
+++ LList/DLList/ops/insert.c
@@ -19,16 +19,6 @@
 #include<stdlib.h>		//For DMA functions like malloc(), free(),....
 
 
-/** ===== Function Prototypes ===== **/
-int create(void);
-int init(void);
-int display(void);
-int insertMain(void);
-int insertFront(void);
-int insertPos(void);
-int insertEnd(void);
-
-
 /** ===== Global Declaration ===== **/
 /* ---- Node Definition ---- */
 struct node {
@@ -37,109 +27,153 @@ struct node {
 	struct node *next;
 };
 
-struct node header, *N = NULL, new;		//Declaraing here for global access.
+struct node *header = NULL, *N = NULL, *new = NULL;		//Declaraing here for global access.
 int n;
+
+
+/** ===== Function Prototypes ===== **/
+struct node* createNode(int value);
+void createList(int n);
+void freeList(void);
+int display(struct node *);
+int insertMain(void);
+int insertFront(void);
+int insertPos(void);
+int insertEnd(void);
 
 
 /** ===== Main Function ===== **/
 int main()
 {
-	if(create()) return 1;
-	init();
+	header = malloc(sizeof(struct node));
+	if(header == NULL)
+	{
+		printf("Memory allocation failed!\n");
+		return 1;
+	}
+
+	header->next = NULL;
+	printf("\nEnter the no. of nodes of the list: ");
+	scanf("%d", &n);
+	if(n < 1)
+	{
+		printf("\nInsufficient nodes to create a list!!\n\n");
+		free(header); header = NULL;
+		return 1;
+	}
+
+	printf("\nEnter the data of all of the nodes: ");
+	createList(n);
 	printf("\nThe List is : ");
-	if(display()) return 1;
+	if(display(header)) return 1;
 	if(insertMain()) return 1;
+
+	//free the allocated memory:
+	freeList();
+	free(header);  header = NULL;
 
 	return 0;
 }
 
 
 /** ===== Function Definitions ===== **/
-//create():
-//This function allocates the memory for the nodes.
+//createNode():
+//This function allocates memory for a single node.
 
-int create()
+struct node* createNode(int value)
 {
-	//Taking the total no. of nodes from the user:
-	printf("\nEnter the no. of nodes of the list: ");
-	scanf("%d", &n);
+	struct node *newNode = (struct node*)malloc(sizeof(struct node));
 
-	if(n < 1)
+	if(newNode == NULL)
 	{
-		printf("\nInsufficient nodes to create a list!!\n\n");		//Handling edge cases.
-		return 1;
+		printf("Memory allocation failed!\n");
+		return NULL;
 	}
 
-	//Allocating the memory for the n nodes.
-	N = malloc(n * sizeof(struct node));
+	newNode->data = value;
+	newNode->prev = NULL;
+	newNode->next = NULL;
 
-	if(N == NULL)
-	{
-		printf("\nMemory allocation failed!");		//Checking if memory allocation is done or not.
-		return 1;				
-	}
-
-	return 0;
+	return newNode;
 }
 
 
-//init():
-//This function initializes all of the nodes in the list with a certain values.
+//createList():
+//This function initializes the list with user-provided values.
 
-int init()
+void createList(int n)
 {
-	struct node *temp = NULL;
-	int i = 0;
+	struct node *temp = NULL, *last = NULL;
 
-	//Linking header node to the N nodes.
-	header.next = &N[0];
-
-	//Taking the data of all the nodes.
-	printf("\nEnter the data of all of the nodes: ");
-	while(i < n)
+	for(int i = 0; i < n; i++)
 	{
-		scanf("%d", &((N+i) -> data));
-		
-		if(i == n-1)
+		int val;
+		scanf("%d", &val);
+
+		struct node *newNode = createNode(val);
+		if(newNode == NULL)
 		{
-			(N+i) -> next = NULL;
-			return 0;
+			freeList();
+			return;
 		}
 
-		(N+i) -> prev = temp;
+		if(header->next == NULL)
+		{
+			header->next = newNode;
+		}
+		else
+		{
+			newNode -> prev = last;
+			last->next = newNode;
+		}
 
-		(N+i) -> next = ((N+i)+1);
-		i += 1;
-		temp = (N+i);
+		last = newNode;
+		temp = newNode;
 	}
 
-	printf("\nSuccessfully initialized all the nodes with data.\n\n");
+	if(temp != NULL)
+	{
+		temp->next = NULL;
+	}
+}
 
-	return 0;
+
+void freeList(void)
+{
+	struct node *curr = header->next;
+	while(curr != NULL)
+	{
+		struct node *next = curr->next;
+		free(curr);
+		curr = next;
+	}
+
+	header->next = NULL;
+	new = NULL;
 }
 
 
 //display():
 //This function displays all of the nodes and it's data.
 
-int display()
+int display(struct node *head)
 {
 	//Checking the edge case:
-	if(header.next == NULL)
+	if(head -> next == NULL)
 	{
 		printf("\nThe list is empty!!\n\n");
 		return 1;
 	}
 
-	struct node *temp = header.next;
+	struct node *temp = head -> next;
 
 	//Traversing through every node and printing their data values.
 	while(temp != NULL)
 	{
-		printf("[ %d ] -> ", temp -> data);
+		printf("[ %d ] -> <- ", temp -> data);
 		temp = temp -> next;
 	}
-	printf(" [ NULL ]");
+	printf("\b\b [ NULL ]");
 
 	printf("\nSuccessfully printed the list.\n\n");
 
@@ -156,8 +190,10 @@ int insertMain()
 
 	//Creating the new node that is to be inserted:	
 	printf("\nEnter the data value of the new node that is to be inserted: ");
-	scanf("%d", &new.data);
-	new.next = NULL;
+	int newValue;
+	scanf("%d", &newValue);
+	new = createNode(newValue);
+	if(new == NULL) return 1;
 	printf("\nSuccessfully created the new node.\n");
 
 	//Taking the user's choice at which position should the node be inserted.
@@ -196,17 +232,17 @@ int insertFront()
 {
 	struct node *temp;
 
-	temp = header.next;
+	temp = header -> next;
 
 	//Linking the new node to the header node.
-	header.next = &new;
+	header -> next = new;
 
 	//Linking the list to the new node that is inserted at the front position.
-	new.next = temp;
-	new.prev = NULL;
+	new->next = temp;
+	new->prev = NULL;
 
 	//Display the list after the insertion:
-	if(display())  return 1;
+	if(display(header))  return 1;
 
 	return 0;
 }
@@ -240,7 +276,7 @@ int insertPos()
 	
 	struct node *temp = NULL, *prev = NULL;
 
-	temp = header.next;
+	temp = header -> next;
 
 	while(temp != NULL && j < pos)
 	{
@@ -250,15 +286,14 @@ int insertPos()
 	}
 	
 	//Linking the new node to the node befor position 'pos':
-	prev -> next = &new;
+	prev -> next = new;
 
 	//Linking the nodes from the position 'pos' to the the new node:
-	new.next = temp;
-	new.prev = prev;
-	temp -> prev = &new;
+	new->next = temp;
+	new->prev = prev;
 
 	//Display the modified list:
-	display();
+	if(display(header))  return 1;
 
 	return 0;
 }
@@ -269,24 +304,25 @@ int insertPos()
 
 int insertEnd()
 {
-	struct node *temp;
+	struct node *temp = NULL, *prev = NULL;
 
-	temp = header.next;
+	temp = header -> next;
 
 
 	//Traversing till the last node:
 	while(temp -> next != NULL)
 	{
+		prev = temp;
 		temp = temp -> next;
 	}
 
 	//Inserting the node at the end:
-	temp -> next = &new;
-	new.next = NULL;
-	new.prev = temp;
+	temp -> next = new;
+	new->next = NULL;
+	new->prev = prev;
 
 	//Display the new list:
-	if(display())  return 1;
+	if(display(header))  return 1;
 
 	return 0;
 		
